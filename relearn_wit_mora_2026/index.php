6d8b930bf1c98c278bb752b08dd3fe2d8fd744c3
Kernels-sws-com <kernels.sws@gmail.com>
2026-07-05 20:29:21 +0300

    new commit

--- relearn_wit_mora_2026/index.php
+++ relearn_wit_mora_2026/index.php
@@ -1,12 +1,37 @@
 <?php
 echo "Hello world";
+echo "<br>";
 
-print("<br>Hello man");
+
+
+
+
+
+
+
+
+
+
 
 
 // ==== Summary Comment ==== //
 // echo "Text";
 // print("Text");
+// echo "Current PHP version: " . phpversion();
+
 // --- variables --- //
-// $name = "value";
+// $name = "value"; \\string var
+// $name = 10; \\ int var
+// $name = 15.50 ; \\ float var
+// $name = true ; \\ boolian var
+// 
+
+// --- comment types --- //
+// 1- (// ) 2- (# ) 3- (/* */)
+
+// --- Data types --- //
+//
+
 ?>
+
+<?= '<br> print this string' ?>
