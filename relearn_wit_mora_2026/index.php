94fc920bd4011e1b1518fb9a0657fdfd3f1ae6c2
Kernels-sws-com <kernels.sws@gmail.com>
2026-07-10 23:34:07 +0300

    new commit

--- relearn_wit_mora_2026/index.php
+++ relearn_wit_mora_2026/index.php
@@ -14,21 +14,45 @@ echo "<br>";
 
 
 // ==== Summary Comment ==== //
+// you can vesit 'w3schools.com' to know more.
+
+// 1--- print php code --- //
 // echo "Text";
 // print("Text");
 // echo "Current PHP version: " . phpversion();
 
-// --- variables --- //
+// 2--- variables --- //
 // $name = "value"; 
 
-// --- comment types --- //
+// 3--- comment types --- //
 // 1- (// ) 2- (# ) 3- (/* */)
 
-// --- Data types --- //
+// 4--- Data types --- //
 // $name = "Text"; \\string var
 // $name = 10; \\ int var
 // $name = 15.50 ; \\ float var
-// $name = true ; \\ boolian var
+// $name = true ; \\ boolean var
+// var_dump($name);
+
+// 5--- Constants variable--- //
+// define("varName", "varValue", 'case_insensitive default is'(false));
+
+// 6--- Operators --- //
+// + , - , * , / 
+// += , -= , *= , /= . (for making calculations on the value of the same var.)
+// == , !== . (equal or not equal.)
+
+// 7--- if, else, ifelse --- //
+// if($name == "value"){do some action} else{do alternative action}
+// if($name == "value"){do some action} elseif($name == "other value"){do an other action} ... else{do final action}
+
+// 8--- while loop --- //
+// $name="value"; while($name <= "value"){ echo "..."; $name++; }
+
+// 9--- do while loop --- //
+// $name="value"; do{do some action} while($name <= "value");
+
+// 10--- for loop --- //
 // 
 
 ?>
